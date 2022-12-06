2e064f38acec3799c0b689382a846a1699794f83
ynguyenntc <ynguyen.ntc@gmail.com>
2022-12-06 23:46:00 +0700

    add

---
+++ tokenizer_vncore.py
@@ -0,0 +1,39 @@
+from vncorenlp import VnCoreNLP
+rdrsegmenter = VnCoreNLP("vncorenlp/VnCoreNLP-1.1.1.jar",
+                         annotators="wseg",
+                         max_heap_size='-Xmx1g')
+
+
+def wordTokenzizer(sentence):
+    '''
+    Tach tu voi thu vien VnCoreNLP
+    '''
+    token = [i for j in rdrsegmenter.tokenize(sentence) for i in j]
+    return ' '.join(token)
+
+
+def wordsTokenzizer(path2Raw='input/data_raw.txt'):
+    '''
+    Tach nhieu cau thanh tu voi thu vien VnCoreNLP
+    '''
+    list_Tokens = []
+    list_LinesTokens = []
+    with open(path2Raw, 'r', encoding="utf-8") as f:
+        data = f.read()
+
+        list_Lines = data.split('\n')
+        for line in list_Lines:
+            token = [i for j in rdrsegmenter.tokenize(line) for i in j]
+            list_LinesTokens.append(' '.join(token))
+            list_Tokens.append(token)
+
+    return list_Tokens, list_LinesTokens
+
+
+list_Tokens, list_LinesTokens = wordsTokenzizer()
+#pickle.dump(list_Tokens, open('output/VnCore_Tokens.pkl', 'wb'))
+
+with open('input\data_tokenizer_vncorenlp.txt', 'w', encoding="utf-8") as f:
+    for line in list_LinesTokens:
+        f.write(line)
+        f.write('\n')
